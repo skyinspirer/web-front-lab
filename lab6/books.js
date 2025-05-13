b02f9141e854b8551680f100175ce34922d47177
Цеунов Матвей <matvey1996007sport@gmail.com>
2025-05-13 21:57:27 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Федор Достоевский',
+        name: 'Братья Карамазовы',
+        publicationYear: 1880,
+        price: '(700₽)',
+        bestseller: false,
+        available: true,
+    },
+    {
+        author: 'Шарлотта Бронте',
+        name: 'Джейн Эйр',
+        publicationYear: 1847,
+        price: '(600₽)',
+        bestseller: true,
+        available: true,
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Анна Каренина',
+        publicationYear: 1877,
+        price: '(750₽)',
+        bestseller: false,
+        available: true,
+    },
+    {  
+        author: "Уильям Шекспир", 
+        name: "Гамлет", 
+        publicationYear: 1609, 
+        price: "(800₽)", 
+        bestseller: true, 
+        available: false 
+    },
+    { 
+        author: "Дж.К. Роулинг", 
+        name: "Гарри Поттер и Тайная комната", 
+        publicationYear: 1998, 
+        price: "(600₽)", 
+        bestseller: false, 
+        available: false 
+    },
+    { 
+        author: "Виктор Гюго", 
+        name: "Отверженные", 
+        publicationYear: 1862, 
+        price: "(800₽)", 
+        bestseller: false, 
+        available: true 
+    },
+    { 
+        author: "Рэй Брэдбери", 
+        name: "Марсианские хроники", 
+        publicationYear: 1950, 
+        price: "(500₽)", 
+        bestseller: false,
+        available: true 
+    },
+    { 
+        author: "Эрнест Хемингуэй", 
+        name: "По ком звонит колокол", 
+        publicationYear: 1940, 
+        price: "(600₽)", 
+        bestseller: false, 
+        available: false 
+    },
+    { 
+        author: "Халед Хоссейни", 
+        name: "Аэрофлот", 
+        publicationYear: 2010, 
+        price: "(700₽)", 
+        bestseller: true, 
+        available: true 
+    },
+    { 
+        author: "Чак Паланик", 
+        name: "Бойцовский клуб", 
+        publicationYear: 1996, 
+        price: "(650₽)", 
+        bestseller: false, 
+        available: true 
+    }
+];
